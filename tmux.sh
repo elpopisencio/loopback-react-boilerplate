8e1df5bd67716d48aa6f786d5cf10a3dc4805c25
Jorge Luis Beroisa <jberoisa@gmail.com>
2019-11-07 16:40:04 -0300

    add zshrc alias

--- tmux.sh
+++ tmux.sh
@@ -8,3 +8,5 @@ tmux new-window -t $SESSION:1 -n 'frontend'
 tmux send-keys "cd ./client && npm start" C-m
 tmux new-window -t $SESSION:2 -n 'backend'
 tmux send-keys "npx nodemon index.js" C-m
+
+# echo alias boilerplate="cd $PWD && ./tmux.sh" >> ~/.zshrc
